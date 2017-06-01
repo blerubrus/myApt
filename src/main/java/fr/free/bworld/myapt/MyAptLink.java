6ef042953b87f79ef62e7414482c100c49a7b7ff
Benoit Le Rubrus <benoit.le-rubrus@imag.fr>
2017-06-01 15:17:29 +0200

    myAptLink now handles URL starting with http!!! Provide the program with an URL and optionally a label, it will output a link in apt format to this URL then the date between parenthesis.

--- src/main/java/fr/free/bworld/myapt/MyAptLink.java
+++ src/main/java/fr/free/bworld/myapt/MyAptLink.java
@@ -4,6 +4,9 @@
 package fr.free.bworld.myapt;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import fr.free.bworld.myapt.images.BrowseImageDir;
@@ -20,7 +23,7 @@ import fr.free.bworld.myapt.resources.ResourceImpl;
  */
 public class MyAptLink implements GenerateApt {
 	
-	/**The input  path to the resource file or directory.*/
+	/**The input  path to the resource file or directory. Since 20170601, can also be an http(s).*/
 	private String linkTo;
 	
 	/**The label requested for the link.*/
@@ -35,6 +38,9 @@ public class MyAptLink implements GenerateApt {
 	public MyAptLink(String linkTo, String linkLabel) {
 		if (linkTo == null){
 			throw new IllegalArgumentException("myAptLink expects at least one argument: the path to a file or directory...");
+		}
+		else if (linkTo.startsWith("http")){
+			this.linkTo = linkTo;
 		}
 		else{//checks the dir or file exists
 			File f = new File(linkTo);
@@ -86,21 +92,26 @@ public class MyAptLink implements GenerateApt {
 	 */
 	@Override
 	public boolean generate() {
-		File f = new File(linkTo);
-		if (f.isDirectory()){
-			Inspect inspector = new InspectImpl(linkTo);
-			String listing = inspector.toApt();
-			System.out.println(listing);
+		if (linkTo.startsWith("http")){
+			System.out.println(getAptHref());
 		}
-		else{// file
-			Resource res = new ResourceImpl(f.getParent(), f.getName(), linkLabel);
-			System.out.println(res.getAptLink());
-			// if image file
-			for (String extension : BrowseImageDir.SUPPORTED_FORMATS){
-				if (f.getName().endsWith(extension)){
-					System.out.println();
-					System.out.println();
-					System.out.println(res.getAptImageDisplay());
+		else{
+			File f = new File(linkTo);
+			if (f.isDirectory()){
+				Inspect inspector = new InspectImpl(linkTo);
+				String listing = inspector.toApt();
+				System.out.println(listing);
+			}
+			else{// file
+				Resource res = new ResourceImpl(f.getParent(), f.getName(), linkLabel);
+				System.out.println(res.getAptLink());
+				// if image file
+				for (String extension : BrowseImageDir.SUPPORTED_FORMATS){
+					if (f.getName().endsWith(extension)){
+						System.out.println();
+						System.out.println();
+						System.out.println(res.getAptImageDisplay());
+					}
 				}
 			}
 		}
@@ -143,6 +154,38 @@ public class MyAptLink implements GenerateApt {
 
 	}
 
-	
+	/**
+	 * Checks that the "linkTo" starts with "http", else returns null. Else, builds the apt code for this link,
+	 * if the label is null or empty, only the link. Then adds the date in parenthesis.
+	 * then adds the date between parenthesis. Example: 
+	 * @return null if the linkTo does not start with http, or the APT code for a link to this hrefa 
+	 */
+	public String getAptHref(){
+		String result = null;
+		if (linkTo.startsWith("http")){
+			StringBuffer buf = new StringBuffer();
+			buf.append(" {{");
+			if (linkLabel == null || linkLabel.trim().isEmpty()){
+				buf.append(linkTo);
+				buf.append("}}");
+			}
+			else{
+				buf.append("{");
+				buf.append(linkTo);
+				buf.append("}");
+				buf.append(linkLabel);
+				buf.append("}}");
+			}
+			// displays the current date in yyyy-MM-dd between parenthesis
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar cal = Calendar.getInstance();
+			Date date = cal.getTime();
+			buf.append(" (");
+			buf.append(sdf.format(date));
+			buf.append(")");
+			result = buf.toString();
+		}
+		return result;
+	}
 
 }
--- src/test/java/fr/free/bworld/myapt/MyAptLinkJunitTest.java
+++ src/test/java/fr/free/bworld/myapt/MyAptLinkJunitTest.java
@@ -3,16 +3,22 @@
  */
 package fr.free.bworld.myapt;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * TODO: not real tests, just exe
+ * Note that many of the tests here are just for the display of the result...
  * 
  * @author blerubrus
  *
  */
 public class MyAptLinkJunitTest {
+	
+	/**Computes the current date for the myAptLink tests.*/
+	private static final String CURRENT_DATE = new SimpleDateFormat("yyyy-MM-dd").format(Calendar.getInstance().getTime());
 	
 	@Test
 	public void testFile(){
@@ -36,6 +42,60 @@ public class MyAptLinkJunitTest {
 	public void testAptIndexLabel(){
 		MyAptLink mal = new MyAptLink("src/site/apt/index.apt", "test");
 		Assert.assertTrue("bad generate...", mal.generate());
+	}
+	
+	/**
+	 * Example of a linkTo that does not start with http: null expected.
+	 */
+	@Test
+	public void testGetAptHref_noHttp(){
+		MyAptLink mal = new MyAptLink("src/site/apt/index.apt", "test");
+		String actual = mal.getAptHref();
+		Assert.assertNull("should be null for no start with http", actual);
+	}
+	
+	/**
+	 * Nominal case with a label and http link.
+	 */
+	@Test
+	public void testGetAptHref_http(){
+		MyAptLink mal = new MyAptLink("http://coucou", "test");
+		String actual = mal.getAptHref();
+		String expected = " {{{http://coucou}test}} (" + CURRENT_DATE + ")";
+		Assert.assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Case for an http link with a null label .
+	 */
+	@Test
+	public void testGetAptHref_http_nullLabel(){
+		MyAptLink mal = new MyAptLink("http://coucou", null);
+		String actual = mal.getAptHref();
+		String expected = " {{http://coucou}} (" + CURRENT_DATE + ")";
+		Assert.assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Case for an http link with an empty label .
+	 */
+	@Test
+	public void testGetAptHref_http_emptyLabel(){
+		MyAptLink mal = new MyAptLink("http://coucou", "  ");
+		String actual = mal.getAptHref();
+		String expected = " {{http://coucou}} (" + CURRENT_DATE + ")";
+		Assert.assertEquals(expected, actual);		
+	}
+	
+	/**
+	 * Tests the case of an https link.
+	 */
+	@Test
+	public void testGetAptHref_https(){
+		MyAptLink mal = new MyAptLink("https://titi", "");
+		String actual = mal.getAptHref();
+		String expected = " {{https://titi}} (" + CURRENT_DATE + ")";
+		Assert.assertEquals(expected, actual);
 	}
 
 }
